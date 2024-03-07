4809ae0daa7a8d565166b8509a87f7db928e06f1
Ibrahim Hafez <ibrahimhafez1@yahoo.com>
2024-03-07 22:10:36 +0200

    server endpoints added

---
+++ backend/middleware/ensureAuthenticated.js
@@ -0,0 +1,6 @@
+export async functions ensureAuthenticated(req, res, next){
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect(process.env.CLIENT_BASE_URL + "/login")
+}
